91604951c697957144eb6ea41de5a43e5b33fdb4
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-03-30 13:12:22 +0530

    Time: 42 ms (29.96%), Space: 17.9 MB (33.6%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -1,6 +1,6 @@
 class Solution:
     def mergeAlternately(self, w1: str, w2: str) -> str:
-        if len(w1)>len(w2):
+        if len(w1)>=len(w2):
             ans=""
             for i in range(len(w1)):
                 ans=ans+w1[i]
@@ -14,9 +14,9 @@ class Solution:
                     ans=ans+w1[i]
                 ans=ans+w2[i]
             return ans
-        else:
-            ans=""
-            for i,j in zip(w1,w2):
-                ans=ans+i
-                ans=ans+j
-            return ans
+        # else:
+        #     ans=""
+        #     for i,j in zip(w1,w2):
+        #         ans=ans+i
+        #         ans=ans+j
+        #     return ans
