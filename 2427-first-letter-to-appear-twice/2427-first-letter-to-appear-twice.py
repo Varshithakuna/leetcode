a4d096322cf54bdd14564f9db8e7fcb78b00eb27
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-04-26 13:43:37 +0530

    Time: 0 ms (100%), Space: 17.7 MB (74.65%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -0,0 +1,18 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        d={}
+        for i in s:
+            if i in d:
+                d[i]=d[i]+1
+                if d[i]==2:
+                    return i
+            else:
+                d[i]=1
+
+            
+                
+
+    
+
+
+
