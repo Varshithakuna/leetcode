f07346ab1627badd8da5865af4312007b8b4a545
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-05-01 15:42:03 +0530

    Time: 2 ms (32.87%), Space: 18 MB (20.73%) - LeetHub

---
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.py
@@ -0,0 +1,37 @@
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        s=[]
+        k=[]
+        for i in word1:
+            if i not in s:
+                s.append(i)
+        for i in word2:
+            if i not in k:
+                k.append(i)
+        ans=0
+        ans2=0
+        print(k)
+        print(s)
+        for i in s:
+            m=word1.count(i)
+            n=word2.count(i)
+            if (m-n)<=3:
+                ans=ans+1
+            else:
+                return  False
+        for  i in k:
+            m =word1.count(i)
+            n=word2.count(i)
+            if abs(m-n)<=3:
+                ans2=ans2+1
+            else:
+                return False
+        
+        print(len(s))
+        print(ans)
+        if ans==len(s):
+            return True
+        else:
+            return False
+
+        
