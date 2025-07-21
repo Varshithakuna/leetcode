51856dfa182a820af6a66cf1a112c0277dd29e58
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-07-21 16:20:57 +0530

    Time: 307 ms (71.37%), Space: 19.8 MB (25.65%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,24 +1,21 @@
-# class Solution:
-#     def makeFancyString(self, s: str) -> str:
-#         ans=[]
-#         for i in range(len(s)):
-#             if len(ans)>=2 and ans[-1]==s[i] and ans[-2]==s[i]:
-#                 ans.pop()
-#                 ans.append(s[i])
-#                 print(ans)
-#             elif len(ans)>=2 and ans[-1]!=s[i]:
-#                 ans.append(s[i])
-#                 print(ans)
-#             else:
-#                 ans.append(s[i])
-#         return ''.join(ans)
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        result = []
-        for c in s:
-            if len(result) >= 2 and result[-1] == c and result[-2] == c:
+        ans=[]
+        for i in s:
+            if len(ans)>=2 and ans[-1]==i and ans[-2]==i:
                 continue
-            result.append(c)
-        return ''.join(result)
+                print(ans)
+            else:
+                ans.append(i)
+        return ''.join(ans)
+# class Solution:
+#     def makeFancyString(self, s: str) -> str:
+#         result = []
+#         for c in s:
+#             if len(result) >= 2 and result[-1] == c and result[-2] == c:
+#                 continue
+#                 print(result)
+#             result.append(c)
+#         return ''.join(result)
 
         
