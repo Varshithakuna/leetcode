107245e259e6f701200acfaf6701492c308f0ccb
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-08-27 12:36:03 +0530

    Time: 958 ms (11.11%), Space: 32.7 MB (6.52%) - LeetHub

---
+++ 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        ans=[]
+        nums=sorted(nums)
+        for i in range(len(nums)):
+            k=nums[i]+nums[len(nums)-i-1]
+            ans.append(k)
+        print(ans)
+        return max(ans)
+        
