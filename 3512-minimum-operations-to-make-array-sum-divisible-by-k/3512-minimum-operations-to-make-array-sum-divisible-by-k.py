45aad229da62fa3265b9e292e7670e356272c923
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-11-23 19:29:54 +0530

    Time: 1 ms (45.03%), Space: 18.1 MB (29.66%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.py
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -3,15 +3,13 @@ class Solution:
         c=0
         m=sum(nums)
         n=m
-        if n%k==0:
-            return c
-        else:
-            for i in range(m):
-                n=n-1
+        for i in range(m+1):
+            if n%k==0:
+                return c
+                break
+            else:
                 c=c+1
-                if n%k==0:
-                    return c
-                    break
+                n=n-1
 
 
-        
+    
