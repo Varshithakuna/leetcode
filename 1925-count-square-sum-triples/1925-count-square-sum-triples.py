a11c83f82ee7a321c46eb50b42c6a534d73fa557
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-12-08 10:44:34 +0530

    Time: 9866 ms (5.17%), Space: 17.6 MB (94.57%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -4,8 +4,8 @@ class Solution:
         for i in range(1,n+1):
             for j in range(2,n+1):
                 for k in range(j,n+1):
-                    if ((j*j)+(k*k)==(i*i)):
+                    if ((j*j)+(i*i)==(k*k)):
                         ans=ans+1
-        return ans+ans
+        return ans
 
         
