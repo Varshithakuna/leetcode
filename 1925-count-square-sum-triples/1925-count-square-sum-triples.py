0c38f15c9e798d911291d6fdc4135d76cc4b34c7
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-12-08 11:02:48 +0530

    Time: 1704 ms (19.02%), Space: 20.7 MB (11.68%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -1,16 +1,21 @@
 class Solution:
     def countTriples(self, n: int) -> int:
         ans=[]
+        res=[]
         for i in range(1,n+1):
             ans.append(i*i)
-        print(ans)
         c=0
         for i in range(1,n+1):
-            for j in range(1,i+1):
-                if (i*i)+(j*j) in ans:
-                    c=c+1
-        print(c)
-        return c+c
+            for j in range(1,n+1):
+                r=((i*i)+(j*j))
+                if r<=n*n:
+                    res.append(r)
+        print(ans)
+        print(res)
+        for i in res:
+            if i in ans:
+                c=c+1
+        return c
 
 
         
