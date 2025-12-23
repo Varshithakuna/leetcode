b339e579857ee731b776cc503e682d3e5326cc14
Varshithakuna <157210916+Varshithakuna@users.noreply.github.com>
2025-12-23 21:07:13 +0530

    Time: 275 ms (5.43%), Space: 19.4 MB (5.43%) - LeetHub

--- 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.py
+++ 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.py
@@ -7,9 +7,18 @@ class Solution:
                 temp=sorted(temp)
                 arr.append(temp)
         c=0
+        d={}
         for i in arr:
             s=set(i)
-            c=c+len(s)*len(s)
+            if len(s) not in d:
+                d[len(s)]=1
+            else:
+                d[len(s)]+=1
+        print(d)
+        d=sorted(d.items())
+        print(d)
+        for i in d:
+            c=c+i[1]*(i[0]*i[0])
         return c
 
             
